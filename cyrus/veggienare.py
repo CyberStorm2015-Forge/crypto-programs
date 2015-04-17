6e9be143f83318107dc22d7fe720148b8fc1f100
Alphawild <QuestionableRobot@gmail.com>
2015-04-17 11:53:12 -0500

    Cleaned up code to adhere to standards
    Cleaned up code and ensured that it complies with pep8, pep257, and
    flake8 standards.

--- cyrus/veggienare.py
+++ cyrus/veggienare.py
@@ -10,46 +10,50 @@ import string
 __author__ = "Cyrus Struble"
 __email__ = "QuestionableRobot@gmail.com"
 
+
 def vigenere(data, key, mode):
     """
-    Performs a vigenere cipher on data,
-    given some key, encrypting (1) or
-    decrypting (-1) depending on mode.
+    Encrypt some data with a vigenere cipher.
 
     Keyword arguments:
     data -- the ascii data to be encrypted/decrypted
     key -- the ascii key to encrypt/decrypt the given data
     mode -- whether to encrypt (1) or decrypt (-1) the data
     """
-
     result = ""
-    num_nonchar = 0 # keep track of the number of non-alphabet characters
-    stripped_key = key.replace(" ", "") # remove key whitespace
+    num_nonchar = 0  # keep track of the number of non-alphabet characters
+    stripped_key = key.replace(" ", "")  # remove key whitespace
 
-    if (mode not in { 1, -1 }):
+    if (mode not in {1, -1}):
         print("Invalid mode. Use 1 for encryption or -1 for decryption.")
         quit()
 
     for c in range(0, len(data)):
-            if (not data[c].isalpha()): # if the data isn't alphabetic
+            if (not data[c].isalpha()):  # if the data isn't alphabetic
                     result += data[c]
                     num_nonchar += 1
             elif (data[c].islower()):
                     index_of_char = string.lowercase.index(data[c])
-                    index_of_key = string.lowercase.index(stripped_key[(c - num_nonchar) % len(stripped_key)].lower())
+                    key_alpha = stripped_key[(c - num_nonchar) % len(stripped_key)]
+                    index_of_key = string.lowercase.index(key_alpha.lower())
                     result += string.lowercase[(index_of_char + (mode * index_of_key)) % 26]
             elif (data[c].isupper()):
                     index_of_char = string.uppercase.index(data[c])
-                    index_of_key = string.uppercase.index(stripped_key[(c - num_nonchar) % len(stripped_key)].upper())
+                    key_alpha = stripped_key[(c - num_nonchar) % len(stripped_key)]
+                    index_of_key = string.uppercase.index(key_alpha.upper())
                     result += string.uppercase[(index_of_char + (mode * index_of_key)) % 26]
 
     return(result)
 
+
 if __name__ == '__main__':
-        choices = { "encrypt" : 1, "decrypt" : -1 }
-        parser = argparse.ArgumentParser(description="encrypt/decrypt vigenere cipher messages")
-        parser.add_argument('role', choices=choices, help="whether to encrypt or decrypt")
-        parser.add_argument('key', metavar='KEY', type=str, help="the ciper key to encode/decode with")
+        choices = {"encrypt": 1, "decrypt": -1}
+        parser = argparse.ArgumentParser(description="encrypt/decrypt vigenere \
+                                                        messages")
+        parser.add_argument('role', choices=choices, help="whether to encrypt or\
+                                                        decrypt")
+        parser.add_argument('key', metavar='KEY', type=str, help="the ciper key \
+                                                        to encode/decode with")
         args = parser.parse_args()
         mode = choices[args.role]
         key = args.key
